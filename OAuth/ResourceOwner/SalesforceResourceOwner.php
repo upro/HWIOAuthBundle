d1405d320bc99227b9834b625830ffcaf81567c0
Alexandre Deckner <alexandre.deckner@uzzl.com>
2013-08-13 15:59:53 +0200

    initial commit, untested

---
+++ OAuth/ResourceOwner/SalesforceResourceOwner.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware.Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HWI\Bundle\OAuthBundle\OAuth\ResourceOwner;
+
+/**
+ * SalesforceResourceOwner
+ *
+ * @author Alexandre Deckner <adeckner@u-pro.fr>
+ */
+class SalesforceResourceOwner extends GenericOAuth2ResourceOwner
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $options = array(
+        'authorization_url' => 'https://login.salesforce.com/services/oauth2/authorize',
+        'access_token_url'  => 'https://login.salesforce.com/services/oauth2/token',
+        'infos_url'         => 'https://na1.salesforce.com/services/data/v28.0/chatter/users/me',
+        'scope'             => 'full'
+    );
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $paths = array(
+        'identifier'      => 'id',
+        'nickname'        => 'username',
+        'realname'        => 'name'
+    );
+
+    public function getUserInformation($accessToken)
+    {
+        $url = $this->getOption('infos_url');
+
+        $content = $this->doGetUserInformationRequest($url, array(), $accessToken)->getContent();
+
+        $response = $this->getUserResponse();
+        $response->setResponse($content);
+        $response->setResourceOwner($this);
+        $response->setAccessToken($accessToken);
+
+        return $response;
+    }
+
+    protected function doGetUserInformationRequest($url, array $parameters = array(), $access_token = '')
+    {
+        return $this->httpRequest($url, null, array('Authorization: OAuth ' . $access_token));
+    }
+}
