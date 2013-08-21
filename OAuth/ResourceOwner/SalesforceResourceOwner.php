5ae39d7166f63608f526d3a2c6531accb64722d6
Alexandre Deckner <alexandre.deckner@uzzl.com>
2013-08-21 14:05:45 +0200

    get non email photo

--- OAuth/ResourceOwner/SalesforceResourceOwner.php
+++ OAuth/ResourceOwner/SalesforceResourceOwner.php
@@ -38,7 +38,7 @@ class SalesforceResourceOwner extends GenericOAuth2ResourceOwner
         'nickname'        => 'username',
         'realname'        => 'name',
         'email'           => 'email',
-        'profilepicture'  => 'photo.standardEmailPhotoUrl'
+        'profilepicture'  => 'photo.largePhotoUrl'
     );
 
     /**
